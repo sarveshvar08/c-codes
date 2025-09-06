15e756a1bc4e7bccbb78b7baf9285817cc8e9566
sarveshvar08 <sarveshvar08@gmail.com>
2025-09-06 11:47:50 +0530

    new:No. guessing game

--- next.c
+++ next.c
@@ -1,6 +1,28 @@
 #include<stdio.h>
-int main()
-{
-    printf("Hello, World!!!!!!!!!!!!!");
+#include<stdlib.h>
+#include<time.h>
+int main(){
+    srand(time(NULL));
+    int guess=0,tries=0;
+    int min=10,max=100;
+    int ans=(rand()%(max-min+1))+min;
+    printf("Number Guessing Game\n");
+    do{
+        printf("Enter your guess between %d and %d: ",min,max);
+        scanf("%d",&guess);
+        tries++;
+        if(guess>ans){
+            printf("Too High!\n");
+            max=guess-1;
+        }
+        else if(guess<ans){
+            printf("Too Low!\n");
+            min=guess+1;
+        }
+        else{
+            printf("\nCongratulations! You guessed the number %d in %d tries.\n",ans,tries);
+        }
+    }while(guess!=ans);
     return 0;
 }
+
