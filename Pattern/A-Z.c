3a921acb3228a2b1e659a564da5c17fa35422472
sarveshvar08 <sarveshvar08@gmail.com>
2025-09-15 14:38:47 +0530

    FEAT:added X

--- Pattern/A-Z.c
+++ Pattern/A-Z.c
@@ -64,6 +64,22 @@ int main(){
         printf("\n");
     }
     }
+    else if(ch=='X'){
+        int n;
+        printf("Enter the number of rows: ");
+        scanf("%d",&n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j || i+j==n-1){
+                    printf("* ");
+                }
+                else{
+                    printf("  ");
+                }
+            }
+            printf("\n");
+        }
+    }
     else{
         printf("Invalid character.");
     }
